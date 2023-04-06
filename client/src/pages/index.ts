087c41240991cff7d9493578b37d1835f4924835
Rocky Essel <rockyessel76@gmail.com>
2023-04-06 05:25:52 +0000

    refactor

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,6 +1,6 @@
 export { default as Home } from './home';
 export { default as Explore } from './explore';
 export { default as ExploreDetails } from './explore-details';
-export { default as Dashboard } from './dashboard'
+export { default as Dashboard } from './dashboard';
 export { default as User } from './user';
 export { default as UploadAsset } from './upload-asset';
