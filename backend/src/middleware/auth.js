1909b0b2bee157677bbcd9c0776ee5cad90b8176
Rocky Essel <rockyessel76@gmail.com>
2023-04-07 08:50:07 +0000

    added auth

---
+++ backend/src/middleware/auth.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken');
+const User = require('../model/users.js');
+
+const protect = async (request, response, next) => {
+  try {
+    let token;
+
+    if (
+      request.headers.authorization &&
+      request.headers.authorization.startsWith('Bearer')
+    ) {
+      try {
+        token = request.headers.authorization.split(' ')[1];
+
+        const user = jwt.verify(token, `${process.env.JWT_SECRET_TOKEN}`);
+
+        if (user) {
+          request.user = await User.findById(user?._id).select('-password');
+
+          console.log(user);
+        }
+
+        next();
+      } catch (error) {
+        response.status(401).json({ msg: 'Not Authorized' });
+      }
+    }
+
+    if (!token) {
+      response.status(401);
+      throw new Error('Not Authorized, And Token Not Found');
+    }
+  } catch (error) {
+    response.status(401).json({ msg: 'Not Authorized, And Token Not Found' });
+  }
+};
+
+module.exports = { protect };
